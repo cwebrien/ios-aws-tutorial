9f8196cd80aeed301da4a0083228b1c7763d78b4
Cameron Brien <cbrien@cbrien-macbook-13.local>
2021-11-02 07:53:47 -0400

    Initial project for tutorial

---
+++ iOS AWS Tutorial/iOS AWS Tutorial/iOS_AWS_TutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOS_AWS_TutorialApp.swift
+//  iOS AWS Tutorial
+//
+//  Created by Cameron Brien on 11/2/21.
+//
+
+import SwiftUI
+
+@main
+struct iOS_AWS_TutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
